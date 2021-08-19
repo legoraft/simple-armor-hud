626ca9db2c00c95e409eb4c762833f523cdc7992
LegoRaft <50689727+LegoRaft@users.noreply.github.com>
2021-08-19 15:56:39 +0200

    added itembar to armor

--- src/main/java/net/legoraft/armorhud/mixin/hudMixin.java
+++ src/main/java/net/legoraft/armorhud/mixin/hudMixin.java
@@ -3,6 +3,9 @@ package net.legoraft.armorhud.mixin;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -11,14 +14,16 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(InGameHud.class)
-public class hudMixin {
+public abstract class hudMixin {
 
 	@Shadow @Final private MinecraftClient client;
 	@Shadow private int scaledWidth;
 	@Shadow private int scaledHeight;
 
+	@Shadow protected abstract void renderHotbarItem(int x, int y, float tickDelta, PlayerEntity player, ItemStack stack, int seed);
+
 	@Inject(at = @At("TAIL"), method = "renderHotbar")
-	public void renderArmorHud(float delta, MatrixStack matrices, CallbackInfo ci) {
+	public void renderArmorHud(float tickDelta, MatrixStack matrices, CallbackInfo ci) {
 		assert this.client.player != null;
 
 		int i = this.scaledHeight - 55;
@@ -35,10 +40,10 @@ public class hudMixin {
 		}
 
 //		Render all armor items from player
-		MinecraftClient.getInstance().getItemRenderer().renderInGuiWithOverrides(this.client.player.getInventory().getArmorStack(3), this.scaledWidth / 2 + 18, i);
-		MinecraftClient.getInstance().getItemRenderer().renderGuiItemIcon(this.client.player.getInventory().getArmorStack(2), this.scaledWidth / 2 + 33, i);
-		MinecraftClient.getInstance().getItemRenderer().renderInGui(this.client.player.getInventory().getArmorStack(1), this.scaledWidth / 2 + 48, i);
-		MinecraftClient.getInstance().getItemRenderer().renderInGui(this.client.player.getInventory().getArmorStack(0), this.scaledWidth / 2 + 63, i);
+		this.renderHotbarItem(this.scaledWidth / 2 + 18, i, tickDelta, client.player, this.client.player.getInventory().getArmorStack(3), 1);
+		this.renderHotbarItem(this.scaledWidth / 2 + 33, i, tickDelta, client.player, this.client.player.getInventory().getArmorStack(2), 1);
+		this.renderHotbarItem(this.scaledWidth / 2 + 48, i, tickDelta, client.player, this.client.player.getInventory().getArmorStack(1), 1);
+		this.renderHotbarItem(this.scaledWidth / 2 + 63, i, tickDelta, client.player, this.client.player.getInventory().getArmorStack(0), 1);
 
 	}
 }
