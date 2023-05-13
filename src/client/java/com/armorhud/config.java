5987feaceeaa9bbcd0c0c4ddfbf89e3c61fa9948
Legoraft <50689727+legoraft@users.noreply.github.com>
2023-05-13 12:41:11 +0200

    added double_hotbar config option

--- src/client/java/com/armorhud/config.java
+++ src/client/java/com/armorhud/config.java
@@ -11,15 +11,18 @@ import java.util.Properties;
 public class config {
 
     public static boolean BETTER_MOUNT_HUD = false;
+    public static boolean DOUBLE_HOTBAR = false;
 
     private static final Path CONFIG_PATH = FabricLoader.getInstance().getConfigDir().resolve("armorhud.properties");
 
     public void write(Properties properties) {
         properties.setProperty("better_mount_hud", Boolean.toString(BETTER_MOUNT_HUD));
+        properties.setProperty("double_hotbar", Boolean.toString(DOUBLE_HOTBAR));
     }
 
     public void read(Properties properties) {
         BETTER_MOUNT_HUD =  Boolean.parseBoolean(properties.getProperty("better_mount_hud"));
+        DOUBLE_HOTBAR =  Boolean.parseBoolean(properties.getProperty("double_hotbar"));
     }
 
     public void save() {
