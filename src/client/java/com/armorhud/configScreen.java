268930f4c1386c42a5db415b66e99fee61776586
Legoraft <50689727+legoraft@users.noreply.github.com>
2023-10-23 14:32:23 +0200

    added better mount hud option

--- src/client/java/com/armorhud/configScreen.java
+++ src/client/java/com/armorhud/configScreen.java
@@ -6,8 +6,10 @@ import dev.lambdaurora.spruceui.option.SpruceOption;
 import dev.lambdaurora.spruceui.screen.SpruceScreen;
 import dev.lambdaurora.spruceui.widget.SpruceButtonWidget;
 import dev.lambdaurora.spruceui.widget.container.SpruceOptionListWidget;
+import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.text.Text;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Properties;
 
@@ -16,13 +18,14 @@ public class configScreen extends SpruceScreen {
     private final Screen parent;
 
     private final SpruceOption doubleHotbarToggle;
+    private final SpruceOption betterMountHudToggle;
 
-    public configScreen(Screen screen) {
+    public configScreen(@Nullable Screen parent) {
         super(Text.literal("Armorhud test GUI"));
-        parent = screen;
+        this.parent = parent;
 
-        this.doubleHotbarToggle = new SpruceBooleanOption("Double hotbar", () -> config.DOUBLE_HOTBAR, newValue -> config.DOUBLE_HOTBAR = newValue, Text.literal("Enables double hotbar config"));
-
+        this.doubleHotbarToggle = new SpruceBooleanOption("Double hotbar", () -> config.DOUBLE_HOTBAR, newValue -> config.DOUBLE_HOTBAR = newValue, Text.literal("Toggles double hotbar config"));
+        this.betterMountHudToggle = new SpruceBooleanOption("Better mount HUD", () -> config.BETTER_MOUNT_HUD, newValue -> config.BETTER_MOUNT_HUD = newValue, Text.literal("Toggles the better mount hud config"));
     }
 
     @Override
@@ -31,10 +34,16 @@ public class configScreen extends SpruceScreen {
 
         SpruceOptionListWidget list = new SpruceOptionListWidget(Position.of(0, 34), this.width, this.height - 69);
         list.addOptionEntry(this.doubleHotbarToggle, null);
+        list.addOptionEntry(this.betterMountHudToggle, null);
 
         this.addDrawableChild(list);
 
-        this.addDrawableChild(new SpruceButtonWidget(Position.of(this.width / 2 - 50, this.height - 20), 100, 20, Text.literal("Done"), button -> this.applyChanges()));
+        this.addDrawableChild(new SpruceButtonWidget(Position.of(this.width / 2 - 50, this.height - 30), 100, 20, Text.literal("Done"), button -> this.applyChanges()));
+    }
+
+    @Override
+    public void renderTitle(DrawContext context, int MouseX, int MouseY, float delta) {
+        context.drawCenteredTextWithShadow(this.textRenderer, this.title, this.width / 2, 8, 16777215);
     }
 
     public void applyChanges() {
