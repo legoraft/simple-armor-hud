2828bbbe287453137841268aa54d09a1b7b203eb
legoraft <50689727+legoraft@users.noreply.github.com>
2024-05-31 11:52:53 +0200

    Screen now works with mod menu

--- src/client/java/com/armorhud/util/armorHudModMenu.java
+++ src/client/java/com/armorhud/util/armorHudModMenu.java
@@ -2,14 +2,18 @@ package com.armorhud.util;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
-import net.minecraft.client.MinecraftClient;
 import com.armorhud.config.fabricScreen;
+import net.minecraft.client.gui.screen.Screen;
 
 public class armorHudModMenu implements ModMenuApi {
 
-//    @Override
-//    public ConfigScreenFactory<?> getModConfigScreenFactory() {
-//        return (ConfigScreenFactory<?>) new fabricScreen();
-//    }
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return this::createConfigScreen;
+    }
+
+    private Screen createConfigScreen(Screen parent) {
+        return new fabricScreen(parent);
+    }
 
 }
