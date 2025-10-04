ec433f1833b0defb5c8fca71066104c067ab0e29
moehreag <moehreag@gmail.com>
2025-10-04 15:34:46 +0200

    chore: update to 1.21.9

--- src/client/java/com/armorhud/keyBindings.java
+++ src/client/java/com/armorhud/keyBindings.java
@@ -3,17 +3,19 @@ package com.armorhud;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
+import net.minecraft.util.Identifier;
 
 public class keyBindings {
 
-    public static KeyBinding armorHudToggle;
+	public static KeyBinding armorHudToggle;
 
-    public static void registerKeys() {
-        armorHudToggle = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-                "key.armorhud.armorvisible",
-                InputUtil.UNKNOWN_KEY.getCode(),
-                "category.armorhud.toggles"
-                ));
-    }
+	public static void registerKeys() {
+		final KeyBinding.Category category = KeyBinding.Category.create(Identifier.of("simple-armor-hud", "armorhud.toggles"));
+		armorHudToggle = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+				"key.armorhud.armorvisible",
+				InputUtil.UNKNOWN_KEY.getCode(),
+				category
+		));
+	}
 
 }
