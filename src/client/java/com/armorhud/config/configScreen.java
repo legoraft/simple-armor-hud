87d3423a6ac13da463db346118c94587807c2ef2
rh4htr <276647324+rh4htr@users.noreply.github.com>
2026-04-18 10:51:11 +0300

    Refactor configScreen to extend OptionsSubScreen

--- src/client/java/com/armorhud/config/configScreen.java
+++ src/client/java/com/armorhud/config/configScreen.java
@@ -2,88 +2,91 @@ package com.armorhud.config;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.gui.screen.option.GameOptionsScreen;
-import net.minecraft.client.gui.widget.*;
-import net.minecraft.text.Text;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiGraphicsExtractor;
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.components.CycleButton;
+import net.minecraft.client.gui.components.OptionsList;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.gui.screens.options.OptionsSubScreen;
+import net.minecraft.client.gui.layouts.*;
+import net.minecraft.network.chat.Component;
 
 @Environment(EnvType.CLIENT)
-public class configScreen extends GameOptionsScreen {
+public class configScreen extends OptionsSubScreen {
     private final Screen parent;
 
     public configScreen(Screen parent) {
-        super(parent, MinecraftClient.getInstance().options, Text.translatable("config.title"));
+        super(parent, Minecraft.getInstance().options, Component.translatable("config.title"));
         this.parent = parent;
     }
 
-    public CyclingButtonWidget<?> doubleHotbarToggle;
-    public CyclingButtonWidget<?> betterMountHudToggle;
-    public CyclingButtonWidget<?> armorHudToggle;
-    public CyclingButtonWidget<?> disableArmorBar;
-    public CyclingButtonWidget<?> armorPosition;
-    public CyclingButtonWidget<?> rightToLeftToggle;
-    public CyclingButtonWidget<?> trimEmptySlots;
+    public CycleButton<?> doubleHotbarToggle;
+    public CycleButton<?> betterMountHudToggle;
+    public CycleButton<?> armorHudToggle;
+    public CycleButton<?> disableArmorBar;
+    public CycleButton<?> armorPosition;
+    public CycleButton<?> rightToLeftToggle;
+    public CycleButton<?> trimEmptySlots;
 
-    public ButtonWidget doneButton;
+    public Button doneButton;
 
     @Override
     protected void init() {
-        doubleHotbarToggle = CyclingButtonWidget.onOffBuilder(config.DOUBLE_HOTBAR)
-                .build(Text.translatable("config.doublehotbar"), ((button, value) -> config.DOUBLE_HOTBAR = !config.DOUBLE_HOTBAR));
+        doubleHotbarToggle = CycleButton.onOffBuilder(config.DOUBLE_HOTBAR)
+                .create(Component.translatable("config.doublehotbar"), ((button, value) -> config.DOUBLE_HOTBAR = !config.DOUBLE_HOTBAR));
 
-        betterMountHudToggle = CyclingButtonWidget.onOffBuilder(config.BETTER_MOUNT_HUD)
-                .build(Text.translatable("config.bettermounthud"), (button, value) -> config.BETTER_MOUNT_HUD = !config.BETTER_MOUNT_HUD);
+        betterMountHudToggle = CycleButton.onOffBuilder(config.BETTER_MOUNT_HUD)
+                .create(Component.translatable("config.bettermounthud"), (button, value) -> config.BETTER_MOUNT_HUD = !config.BETTER_MOUNT_HUD);
 
-        armorHudToggle = CyclingButtonWidget.onOffBuilder(config.ARMOR_HUD)
-                .build(Text.translatable("config.armorvisible"), (button, value) -> config.ARMOR_HUD = !config.ARMOR_HUD);
+        armorHudToggle = CycleButton.onOffBuilder(config.ARMOR_HUD)
+                .create(Component.translatable("config.armorvisible"), (button, value) -> config.ARMOR_HUD = !config.ARMOR_HUD);
 
-        disableArmorBar = CyclingButtonWidget.onOffBuilder(config.DISABLE_ARMOR_BAR)
-                .build(Text.translatable("config.disablearmorbar"), ((button, value) -> config.DISABLE_ARMOR_BAR = !config.DISABLE_ARMOR_BAR));
+        disableArmorBar = CycleButton.onOffBuilder(config.DISABLE_ARMOR_BAR)
+                .create(Component.translatable("config.disablearmorbar"), ((button, value) -> config.DISABLE_ARMOR_BAR = !config.DISABLE_ARMOR_BAR));
 
-        armorPosition = CyclingButtonWidget.builder(config.Position::displayName, config.Position.valueOf(String.valueOf(config.position))).values(config.Position.values())
-                .build(Text.translatable("config.armorposition"), ((button, value) -> config.position = value));
+        armorPosition = CycleButton.builder(config.Position::displayName, config.Position.valueOf(String.valueOf(config.position))).withValues(config.Position.values())
+                .create(Component.translatable("config.armorposition"), ((button, value) -> config.position = value));
 
-        rightToLeftToggle = CyclingButtonWidget.onOffBuilder(config.RTL)
-                .build(Text.translatable("config.righttoleft"), (button, value) -> config.RTL = !config.RTL);
+        rightToLeftToggle = CycleButton.onOffBuilder(config.RTL)
+                .create(Component.translatable("config.righttoleft"), (button, value) -> config.RTL = !config.RTL);
 
-        trimEmptySlots = CyclingButtonWidget.onOffBuilder(config.TRIM_EMPTY_SLOTS)
-                .build(Text.translatable("config.trimemptyslots"), ((button, value) -> config.TRIM_EMPTY_SLOTS = !config.TRIM_EMPTY_SLOTS));
+        trimEmptySlots = CycleButton.onOffBuilder(config.TRIM_EMPTY_SLOTS)
+                .create(Component.translatable("config.trimemptyslots"), ((button, value) -> config.TRIM_EMPTY_SLOTS = !config.TRIM_EMPTY_SLOTS));
 
-        doneButton = ButtonWidget
-                .builder(Text.translatable("config.done"), button -> close())
-                .dimensions(width / 2 - 100, height - 25, 200, 20)
+        doneButton = Button
+                .builder(Component.translatable("config.done"), button -> onClose())
+                .bounds(width / 2 - 100, height - 25, 200, 20)
                 .build();
 
-        OptionListWidget optionListWidget = this.addDrawableChild(new OptionListWidget(this.client, this.width, this));
-        optionListWidget.addHeader(Text.translatable("config.header.general"));
-        optionListWidget.addWidgetEntry(armorHudToggle, disableArmorBar);
+        OptionsList optionListWidget = this.addRenderableWidget(new OptionsList(this.minecraft, this.width, this));
+        optionListWidget.addHeader(Component.translatable("config.header.general"));
+        optionListWidget.addSmall(armorHudToggle, disableArmorBar);
 
-        optionListWidget.addHeader(Text.translatable("config.header.compatibility"));
-        optionListWidget.addWidgetEntry(betterMountHudToggle, doubleHotbarToggle);
+        optionListWidget.addHeader(Component.translatable("config.header.compatibility"));
+        optionListWidget.addSmall(betterMountHudToggle, doubleHotbarToggle);
 
-        optionListWidget.addHeader(Text.translatable("config.header.display"));
-        optionListWidget.addWidgetEntry(armorPosition, rightToLeftToggle);
-        optionListWidget.addWidgetEntry(trimEmptySlots, null);
+        optionListWidget.addHeader(Component.translatable("config.header.display"));
+        optionListWidget.addSmall(armorPosition, rightToLeftToggle);
+        optionListWidget.addSmall(trimEmptySlots, null);
 
-        addDrawableChild(doneButton);
+        addRenderableWidget(doneButton);
     }
 
     @Override
     protected void addOptions() { }
 
     @Override
-    public void render(DrawContext context, int mouseX, int mouseY, float delta) {
-        context.drawCenteredTextWithShadow(this.textRenderer, Text.translatable("config.title"), this.width / 2, 12, 0xffffff);
-        super.render(context, mouseX, mouseY, delta);
+    public void extractRenderState(GuiGraphicsExtractor context, int mouseX, int mouseY, float delta) {
+        context.centeredText(this.font, Component.translatable("config.title"), this.width / 2, 12, 0xffffff);
+        super.extractRenderState(context, mouseX, mouseY, delta);
     }
 
     @Override
-    public void close() {
-        assert this.client != null;
+    public void onClose() {
+        assert this.minecraft != null;
 
         config.save();
-        this.client.setScreen(this.parent);
+        this.minecraft.setScreen(this.parent);
     }
 }
