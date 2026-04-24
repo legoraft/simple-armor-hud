3f5cfd9ac793ca956e22607f506b6c2ab04187ba
legoraft <git@legoraft.com>
2026-04-24 16:02:13 +0200

    fix: changed armoraccessor behaviour

--- src/client/java/com/armorhud/armor/ArmorAccessor.java
+++ src/client/java/com/armorhud/armor/ArmorAccessor.java
@@ -2,12 +2,12 @@ package com.armorhud.armor;
 
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.world.entity.EquipmentSlot;
-import net.minecraft.world.item.ItemStackTemplate;
+import net.minecraft.world.item.ItemStack;
 
 public interface ArmorAccessor {
 
     default void initialize(LocalPlayer player) {
     }
 
-    ItemStackTemplate getArmorPiece(LocalPlayer player, EquipmentSlot slot);
+    ItemStack getArmorPiece(LocalPlayer player, EquipmentSlot slot);
 }
